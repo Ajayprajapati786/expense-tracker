d59a5f95350abf3ff354672e01cab304996e8618
Ajay Prajapati <prajapatisani9@gmail.com>
2023-02-27 00:25:54 +0530

    task 3

--- src/components/DailyExpense.js
+++ src/components/DailyExpense.js
@@ -1,10 +1,12 @@
-import React, { useRef } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, { useState, useRef } from 'react';
+import { Form, Button, Table } from 'react-bootstrap';
 
 const DailyExpense = () => {
   const amountRef = useRef();
   const descriptionRef = useRef();
   const categoryRef = useRef();
+
+  const [expenses, setExpenses] = useState([]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -15,7 +17,10 @@ const DailyExpense = () => {
       category: categoryRef.current.value
     };
 
-    console.log(expense); // Here, you can send the expense data to your backend API or do something else with it
+// Here would be the code to send expense to server
+
+    setExpenses([...expenses, expense]); // Add new expense to the list
+
 
     // Clear form fields
     amountRef.current.value = '';
@@ -46,10 +51,28 @@ const DailyExpense = () => {
           </Form.Control>
         </Form.Group>
         <div className='text-center'>
-        <Button variant="primary" type="submit" className='mt-3'>Add Expense</Button>
-
+          <Button variant="primary" type="submit" className='mt-3'>Add Expense</Button>
         </div>
       </Form>
+      <h2 className='text-center mt-5'>Expenses</h2>
+      <Table striped bordered hover className='container'>
+        <thead>
+          <tr>
+            <th>Amount</th>
+            <th>Description</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense, index) => (
+            <tr key={index}>
+              <td>{expense.amount}</td>
+              <td>{expense.description}</td>
+              <td>{expense.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   )
 }
